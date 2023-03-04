007dbb628898021250f81f3dd424dd06f225fafa
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-04 01:07:44 -0300

    First Steps in java Spring Boot 3

---
+++ 05-aula/udemy-api/src/main/java/com/jailson/api/UdemyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jailson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiApplication.class, args);
+	}
+
+}
---
+++ 05-aula/udemy-api/src/test/java/com/jailson/api/UdemyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jailson.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
