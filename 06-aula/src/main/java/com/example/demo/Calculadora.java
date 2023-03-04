cb35edd0a3a466298ed6cd3181c8eaf2b3500b4d
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-04 17:22:08 -0300

    refatorando o codigo

--- 06-aula/src/main/java/com/example/demo/Calculadora.java
+++ 06-aula/src/main/java/com/example/demo/Calculadora.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.converters.NumberConverter;
 import com.example.demo.exceptions.UnsupportrdMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,72 +14,59 @@ public class Calculadora {
     public Double sum(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+        return NumberConverter.covertToDouble(numberOne) + NumberConverter.covertToDouble(numberTwo);
     }
 
     @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double sub(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return covertToDouble(numberOne) - covertToDouble(numberTwo);
+        return NumberConverter.covertToDouble(numberOne) - NumberConverter.covertToDouble(numberTwo);
     }
 
     @RequestMapping(value="/mult/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double mult(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return covertToDouble(numberOne) * covertToDouble(numberTwo);
+        return NumberConverter.covertToDouble(numberOne) * NumberConverter.covertToDouble(numberTwo);
     }
 
     @RequestMapping(value="/div/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double div(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return covertToDouble(numberOne) / covertToDouble(numberTwo);
+        return NumberConverter.covertToDouble(numberOne) / NumberConverter.covertToDouble(numberTwo);
     }
 
     @RequestMapping(value="/media/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double media(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+        return (NumberConverter.covertToDouble(numberOne) + NumberConverter.covertToDouble(numberTwo)) / 2;
     }
 
     @RequestMapping(value="/raiz/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double raiz(@PathVariable("numberOne") String numberOne) throws Exception {
 
-        if (!isNumeric(numberOne)) {
+        if (!NumberConverter.isNumeric(numberOne)) {
             throw new UnsupportrdMathOperationException("Insira um valor numerico");
         }
-        return Math.sqrt(covertToDouble(numberOne));
+        return Math.sqrt(NumberConverter.covertToDouble(numberOne));
     }
 
-    public static Double covertToDouble(String strNumber) {
-        if (strNumber == null) return 0d;
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-        return 1.0d;
-    }
-
-    public static boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-        //return false;
-    }
 }
