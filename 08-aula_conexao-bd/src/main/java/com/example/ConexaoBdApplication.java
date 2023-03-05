aee6e3d98f53609707b15440e0cd032bde3056b6
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-04 22:56:11 -0300

    commit inicial

---
+++ 08-aula_conexao-bd/src/main/java/com/example/ConexaoBdApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoBdApplication.class, args);
+	}
+
+}
