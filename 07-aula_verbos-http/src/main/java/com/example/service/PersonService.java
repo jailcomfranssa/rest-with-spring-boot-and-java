c4257abdb7e9bd442bb9aa22edf54bc467bcca47
jailcomfranssa <jail_son_0@hotmail.com>
2023-03-04 22:26:14 -0300

    criando o GET ALL Person

--- 07-aula_verbos-http/src/main/java/com/example/service/PersonService.java
+++ 07-aula_verbos-http/src/main/java/com/example/service/PersonService.java
@@ -3,6 +3,8 @@ package com.example.service;
 import com.example.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,19 @@ public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
     private final Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<Person> findAll(){
+        logger.info("Finding all person!");
+        List<Person> persons = new ArrayList<>();
+        for(int i =0; i<8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+
+        }
+        return persons;
+    }
+
+
 
     public Person findbyId(String id){
         logger.info("Finding one person!");
@@ -25,5 +40,39 @@ public class PersonService {
         return person;
 
     }
+
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name "+i);
+        person.setLastName("Person LastName "+i);
+        person.setAddress("Person Address "+i);
+        person.setGender("Person Gender" +i);
+
+        return person;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
